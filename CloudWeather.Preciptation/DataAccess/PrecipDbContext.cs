87878e736f062f8aae69e9042fdb2d4292443f58
Khaledov <khaled.m.ramadan@gmail.com>
2022-09-25 01:00:33 +0200

    adding data access to precip service

---
+++ CloudWeather.Preciptation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Preciptation.DataAccess
+{
+    public class PrecipDbContext: DbContext
+    {
+        public PrecipDbContext()
+        {
+
+        }
+
+        public PrecipDbContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b=>
+            {
+                b.ToTable("precipitation");
+            });
+        }
+
+
+    }
+}
---
+++ CloudWeather.Preciptation/DataAccess/Precipitation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CloudWeather.Preciptation.DataAccess
+{
+    public class Precipitation
+    {
+        
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public decimal AmountInches { get; set; }
+        public string? WeatherType { get; set; }
+        public string? ZipCode { get; set; } 
+
+    }
+}
