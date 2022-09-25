b700b057e78f79b76aa821e6bbb5a5367cfa213f
Khaledov <khaled.m.ramadan@gmail.com>
2022-09-25 12:52:58 +0200

    adding endpoints for Temperature service

--- CloudWeather.Temprature/Program.cs
+++ CloudWeather.Temprature/Program.cs
@@ -17,27 +17,27 @@ ServiceLifetime.Transient
 var app = builder.Build();
 
 
-//app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, TempDbContext db) =>
-//{
-//    if (days == null || days < 1 || days > 30)
-//    {
-//        return Results.BadRequest("Please add a 'days' query parameter between 1 and 30");
-//    }
-//    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, TempDbContext db) =>
+{
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please add a 'days' query parameter between 1 and 30");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
 
-//    var result = await db.Temperature
-//    .Where(precipt => precipt.ZipCode == zip && precipt.CreatedOn >= startDate)
-//    .ToListAsync();
+    var result = await db.Temperature
+    .Where(temp => temp.ZipCode == zip && temp.CreatedOn >= startDate)
+    .ToListAsync();
 
-//    return Results.Ok(result);
+    return Results.Ok(result);
 
-//});
+});
 
-//app.MapPost("/observation", async (Precipitation precip, PrecipDbContext db) =>
-//{
-//    precip.CreatedOn = precip.CreatedOn.ToUniversalTime();
-//    await db.AddAsync<Precipitation>(precip);
-//    await db.SaveChangesAsync();
-//});
+app.MapPost("/observation", async (Temperature temp, TempDbContext db) =>
+{
+    temp.CreatedOn = temp.CreatedOn.ToUniversalTime();
+    await db.AddAsync<Temperature>(temp);
+    await db.SaveChangesAsync();
+});
 
 app.Run();
